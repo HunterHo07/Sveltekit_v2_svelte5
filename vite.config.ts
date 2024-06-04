8265b4506fbbe4447918126aaee68d952e79d0e1
Hunter <hunterho.my@gmail.com>
2024-06-05 03:28:09 +0800

    added npm run start for local host expose

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit(), 
-	// 	svelte({
-	// 	/* plugin options */
-	//   })
+	plugins: [
+		sveltekit()
+		// 	svelte({
+		// 	/* plugin options */
+		//   })
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
+	// server: {
+	// 	host: true
+	// }
+	// server: {
+	// 	host: 'localhost',
+	// 	port: 5713
+	// }
 });
